39bd6e63073cb36bd60e0d236d8098c54f75f44f
marcriepen <klever.marc@gmail.com>
2017-11-17 17:48:58 +0100

    adjusted appspec.yml so it moves contents of /public into the a dir named klever.global, otherwise nginx doesnt serve the domain

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -10,9 +10,6 @@ chmod 2775 /var/www
 find /var/www -type d -exec chmod 2775 {} \;
 find /var/www -type f -exec chmod 0664 {} \;
 
-# remove the ".vm" ending on all domains
-find /var/www -maxdepth 1 -type d -exec rename 's/.vm//g' {} \;
-
 # restart php and nginx to clear out php opcache
 sudo service php7.1-fpm restart
 sudo service nginx restart
