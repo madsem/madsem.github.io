c163cc56e9cd425becf12b2e69b4eae94b8ed0ee
madsem <klever.marc@gmail.com>
2020-05-13 10:54:55 +0200

    changed repo around to become a github org page

--- scripts/cleanup.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-###########################################################################
-# clean up old files that codedeploy missed
-# rm -f -R /var/www/*
-# gives error when instace had no files before cleanup, have to make conditional
-###########################################################################
-sudo rm -Rf /var/www/*
